6b41a2ea2eb96e2d5586cb4b5a2e8d911391c136
Rasujon3 <rasujon3@gmail.com>
2022-04-26 21:45:57 +0600

    60-8 Module Summary, Add navbar and set footer dynamic date

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -11,12 +11,14 @@ const Services = () => {
       .then((data) => setServices(data));
   }, []);
   return (
-    <div>
-      <h1 className="services-title my-5">Our Services</h1>
-      <div className="services-container">
-        {services.map((service) => (
-          <Service key={service.id} service={service} />
-        ))}
+    <div className="container">
+      <div className="row">
+        <h1 className="text-primary text-center my-5">Our Services</h1>
+        <div className="services-container">
+          {services.map((service) => (
+            <Service key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   );
